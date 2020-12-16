b5e6ec649dfd7f90babe3c775e7ac0d37fbb53d7
Kedar V <kedarv@users.noreply.github.com>
2020-12-15 21:48:27 -0800

    continue work

--- src/TimelineItem.js
+++ src/TimelineItem.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './index.css';
+import moment from 'moment';
 
 function TimelineItem(props) {
-    console.log(props.offset)
-    return <div className={"timeline-item"} style={{'left': props.offset*117}}>{props.name}</div>;
+    const start = moment(props.start);
+    const end = moment(props.end);
+    const diff = end.diff(start, 'days');
+    // console.log(props.top)
+    let top = props.top > -1 ? props.top : 0;
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return (
+        <div
+            className={"timeline-item"}
+            style={{
+                'left': props.offset * 117,
+                'width': diff * 117,
+                'top': top * 20,
+                'backgroundColor': "#"+randomColor,
+            }}
+        >
+            {props.name}
+        </div>
+    );
 }
 
 export default TimelineItem;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import moment from 'moment';
 class App extends React.Component {
 
   computeBoundaries = () => {
+    // TODO: convert everything to moments
+
     const min = timelineItems.reduce(function (prev, curr) {
       return Date.parse(prev.start) < Date.parse(curr.start) ? prev : curr;
     });
@@ -21,16 +23,46 @@ class App extends React.Component {
     const maxDate = moment(max.end);
     const days = maxDate.diff(minDate, 'days')
 
+    let stackingData = {};
+    for (let i = 0; i < days; i++) {
+      stackingData[minDate.clone().add(i, 'days')] = {};
+    }
+    console.log(stackingData);
+    timelineItems.forEach(element => {
+      const start = moment(element.start);
+      const end = moment(element.end);
+      // go from start to end by adding one day
+      for (let j = 0; j < end.diff(start, 'days'); j++) {
+        const d = start.clone().add(j, 'days');
+        const itemsLength = Object.keys(stackingData[d]).length;
+        if (itemsLength === 0) {
+          console.log("nothing found in " + d.format("YYYY-MM-DD"))
+          stackingData[d][0] = element.id;
+        } else {
+          for (let k = 0; k < itemsLength + 1; k++) {
+            if (!(k in stackingData[d])) {
+              stackingData[d][k] = element.id;
+              break;
+            }
+          }
+        }
+        // console.log(d);
+        console.log(stackingData[d]);
+      }
+    });
+
     return {
       days,
       minDate,
-      maxDate
+      maxDate,
+      stackingData
     };
   }
 
   render() {
-    const { days, minDate, maxDate } = this.computeBoundaries()
+    const { days, minDate, maxDate, stackingData } = this.computeBoundaries()
     let items = [];
+
     for (let i = 0; i < days; i++) {
       items.push(minDate.clone().add(i, 'days'));
     }
@@ -52,7 +84,12 @@ class App extends React.Component {
             <div class="timeline-body">
               <div class="timeline-events">
                 {timelineItems.map((item, index) => {
-                  return <TimelineItem {...item} offset={moment(item.start).diff(minDate, 'days')}></TimelineItem>
+                  return (
+                    <TimelineItem
+                      {...item}
+                      offset={moment(item.start).diff(minDate, 'days')}
+                      top={Object.keys(stackingData[moment(item.start)]).find(key => stackingData[moment(item.start)][key] === item.id)}></TimelineItem>
+                  )
                 })}
 
               </div>
