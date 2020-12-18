21fed5ec1374f113ec2b209a76a5c5ee7e97df58
Kedar V <kedarv@users.noreply.github.com>
2020-12-17 17:54:38 -0800

    more structure changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import ReactDOM from 'react-dom';
 import Timeline from './Timeline';
 
+function App() {
+    return (
+        <>
+            <h2><b>Airtable Timeline</b></h2>
+            <pre>
+                Some notes:
+                <ul>
+                    <li>Scroll the bordered container below horizontally to view the Timeline</li>
+                    <li>Click on Item title to edit, hit enter or click away to save</li>
+                    <li>Hover on title to view title in entirety (if truncated due to length)</li>
+                    <li>Drag either side of an Item to resize and snap to new dates</li>
+                    <li>Try to break something :)</li>
+                </ul>
+            </pre>
+            <Timeline />
+        </>
+    );
+}
+
 ReactDOM.render(
-    <Timeline/>,
+    <App />,
     document.getElementById('root')
-  );
+);
